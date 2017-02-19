a562e818f66a19aee1c914769f109aa387d5d6ea
Giovanni Sferro <agi.novanta@gmail.com>
2017-02-19 13:03:58 -0800

    Intial support for MBC1.

--- src/gb_proc/memory_controller.rs
+++ src/gb_proc/memory_controller.rs
@@ -31,11 +31,8 @@ impl Mbc for Mbc0 {
     }
 
     fn write(&mut self, _: u16, _: u8) {
-        // println!("Writing {:02X} to {:04X}", v, address);
-        // match address {
-        //    0xA000 ... 0xBFFF => self.ram[(address as usize) - 0xA000] = v,
-        //    _ => unimplemented!(),
-        //}
+        // Theoretically not supposed to write to the Mbc0 ROM, but some games do
+        // it anyway.
     }
 }
 
@@ -46,20 +43,26 @@ enum MemoryMode {
     C16_8,
 }
 
+enum MbcMode {
+    Mbc3,
+    Mbc1,
+}
+
 #[allow(dead_code)]
-struct Mbc3 {
+struct Mbc13 {
     selected_bank: usize,
     data: Vec<u8>,
     offset: usize,
     ram: RefCell<File>,
-    memory_mode: MemoryMode,
 
     ram_offset: usize,
     ram_enabled: bool,
+
+    mode: MbcMode,
 }
 
-impl Mbc3 {
-    pub fn new(data: Vec<u8>, mut disk: File) -> Mbc3 {
+impl Mbc13 {
+    pub fn new(data: Vec<u8>, mut disk: File, mode: MbcMode) -> Mbc13 {
         let mut ram = vec![];
         disk.read_to_end(&mut ram).unwrap();
 
@@ -69,15 +72,16 @@ impl Mbc3 {
             disk.write_all(&[0; 32768]).unwrap();
         }
 
-        Mbc3 {
+        Mbc13 {
             selected_bank: 1,
             data: data,
             offset: 0,
             ram: RefCell::new(disk),
-            memory_mode: MemoryMode::C4_32,
 
             ram_offset: 0,
             ram_enabled: false,
+
+            mode: mode,
         }
     }
 
@@ -103,9 +107,31 @@ impl Mbc3 {
 
         buffer[0]
     }
+
+    fn switch_bank_mbc3(&mut self, v: u8) {
+        // Any write to this area will enable the bank of memory contained in v
+        // if 0 is selected we will select 1 because 0 is already available
+        // statically in 0 0000 - 3FFF
+        if v > 0 {
+            self.offset = ((v - 1) as usize) * 0x4000;
+        } else {
+            self.offset = 0x0000;
+        }
+    }
+
+    fn switch_bank_mbc1(&mut self, v: u8) {
+        // There's a bug in MBC1 that makes it so that for v = 0x00, 0x20, 0x40, 0x60
+        // the bank selected is actually the following one (v+1)
+        let v_bug = match v & 0b11111 {
+            0x00 | 0x20 | 0x40 | 0x60 => v + 1,
+            _ => v,
+        };
+
+        self.offset = ((v_bug - 1) as usize) * 0x4000;
+    }
 }
 
-impl Mbc for Mbc3 {
+impl Mbc for Mbc13 {
     fn read(&self, address: u16) -> u8 {
         match address {
             0x0000 ... 0x3FFF => self.data[address as usize],
@@ -125,15 +151,10 @@ impl Mbc for Mbc3 {
                 }
             },
             0x2000 ... 0x3FFF => {
-                // Any write to this area will enable the bank of memory contained in v
-                // if 0 is selected we will select 1 because 0 is already available
-                // statically in 0 0000 - 3FFF
-                if v > 0 {
-                    self.offset = ((v - 1) as usize) * 0x4000;
-                } else {
-                    self.offset = 0x0000;
+                match self.mode {
+                    MbcMode::Mbc1 => self.switch_bank_mbc1(v),
+                    MbcMode::Mbc3 => self.switch_bank_mbc3(v),
                 }
-                // println!("Setting offset to {:06X}", self.offset);
             },
             0x4000 ... 0x5FFF => {
                 // Writing to this area will cause the controller to switch
@@ -148,12 +169,7 @@ impl Mbc for Mbc3 {
                 }
             },
             0x6000 ... 0x7FFF => {
-                // println!("Attempt to write {:02X} to {:04X}", v, address);
-                /* match v & 0x1 {
-                    0x0 => self.memory_mode = MemoryMode::C16_8, // Memory mode 16_8,
-                    0x1 => self.memory_mode = MemoryMode::C4_32,
-                    _   => panic!(),
-                }*/
+                // TODO
             },
             0xA000 ... 0xBFFF => { self.write_ram(address, v); },
             _ => unimplemented!(),
@@ -168,8 +184,12 @@ pub struct MemoryController {
 impl MemoryController {
     pub fn from_bytes(bytes: Vec<u8>, save_file: File) -> MemoryController {
         let controller = match bytes[0x147] {
-            0x00 => Box::new(Mbc0::new(bytes)) as Box<Mbc>,
-            0x0F | 0x10 | 0x11 | 0x12 | 0x13 | 0x01 => Box::new(Mbc3::new(bytes, save_file)) as Box<Mbc>,
+            0x00 =>
+                Box::new(Mbc0::new(bytes)) as Box<Mbc>,
+            0x01 | 0x02 | 0x03 =>
+                Box::new(Mbc13::new(bytes, save_file, MbcMode::Mbc1)) as Box<Mbc>,
+            0x0F | 0x10 | 0x11 | 0x12 | 0x13 =>
+                Box::new(Mbc13::new(bytes, save_file, MbcMode::Mbc3)) as Box<Mbc>,
             _ => {
                 println!("Unrecognized type {:02X}", bytes[0x147]);
                 panic!();
